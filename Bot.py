cecbffcbfd32f38d6bd96dc1153e4420076db491
Alin Basuljevic <41343001+alinbasuljevic@users.noreply.github.com>
2019-09-03 14:06:38 -0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -8,18 +8,6 @@ start_time = time.time()
 def view_item():
     s = requests.Session()
     visiting_url = s.get(ebay_link)
-    
-    headers = {
-        'dnt': '1',
-        'origin': 'https://reg.ebay.com',
-        'referer': 'https://reg.ebay.com/reg/PartialReg?siteid=0&co_partnerId=0&UsingSSL=1&rv4=1&signInUrl=https%3A%2F%2Fwww.ebay.com%2Fsignin%3Fsgn%3Dreg%26siteid%3D0%26co_partnerId%3D0%26UsingSSL%3D1%26rv4%3D1',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
-    }
-    creation_request = s.post(url, data=payload, headers=headers)
-    print (creation_request.status_code)
-
-
 
 for i in range(num_of_views):
     t = threading.Thread(target=view_item)
